94d2245d882fed340c25ea20c0edc0d8ca5b3db9
Stephen Gomez-Fox <stephen@gomezfox.net>
2022-03-13 01:40:41 -0800

    adds custom auth User, manager, and admin page

--- nftgram/urls.py
+++ nftgram/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path, re_path
-from nftgram import views
+from django.urls import path, re_path, include
+from nftgram.views import UserViewSet
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    re_path(r'^users/$', views.user_list),
-    re_path(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- nftgram_api/urls.py
+++ nftgram_api/urls.py
@@ -16,31 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import path, include
-from django.contrib.auth.models import User
+from nftgram.models import User
 from rest_framework import routers, serializers, viewsets
+from nftgram.serializers import UserSerializer
 
 admin.autodiscover()
 
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
+# # Serializers define the API representation.
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
 
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path('api/', include('nftgram.urls')),
     path('admin/', admin.site.urls),
-    path('auth/', include('nftgram.urls')),
     path('login/', views.LoginView.as_view(template_name='nftgram/login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 
